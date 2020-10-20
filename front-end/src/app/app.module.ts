b0c8b56878aab688347c50f8ead42ac8a1fe6235
douglasandradeo <douglasandradeoliv10@gmail.com>
2020-10-20 01:32:05 +0000

    (19/10) Adicionada barra superior.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderBarComponent } from './ui/header-bar/header-bar.component';
+import { MaterialModule } from './material/material.module';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent,
-    HeaderBarComponent
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
